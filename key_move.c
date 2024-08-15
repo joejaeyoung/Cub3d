9ac99bdb956127cb1bfd159fa99b85fea2c90a3b
Joonwan Jeon <joojeon@c3r17s3.42gyeongsan.kr>
2024-08-15 19:09:40 +0900

    dev : cannot go to over wall

--- key_move.c
+++ key_move.c
@@ -3,27 +3,60 @@
 
 void	move_up(t_cub3d *cub3d)
 {
-	cub3d -> user.x += cub3d -> user.dx * 0.1;
-	cub3d -> user.y += cub3d -> user.dy * 0.1;
+	int	nx;
+	int	ny;
+
+	nx = (int)(cub3d -> user.x + cub3d -> user.dx * 0.1 / cub3d -> map.tile_len);
+	ny = (int)(cub3d -> user.y + cub3d -> user.dy * 0.1 / cub3d -> map.tile_len);
+	if (cub3d -> map.array_map[ny][nx] != 1)
+	{
+		cub3d -> user.x += cub3d -> user.dx * 0.1;
+		cub3d -> user.y += cub3d -> user.dy * 0.1;
+	}
+	
 }
 
 void move_down(t_cub3d *cub3d)
 {
-	cub3d -> user.x -= cub3d -> user.dx * 0.1;
-	cub3d -> user.y -= cub3d -> user.dy * 0.1;
+	int	nx;
+	int	ny;
+
+	nx = (int) (cub3d -> user.x - cub3d -> user.dx * 0.1 / cub3d -> map.tile_len);
+	ny = (int) (cub3d -> user.y - cub3d -> user.dy * 0.1 / cub3d -> map.tile_len);
+	if (cub3d -> map.array_map[ny][nx] != 1)
+	{
+		cub3d -> user.x -= cub3d -> user.dx * 0.1;
+		cub3d -> user.y -= cub3d -> user.dy * 0.1;
+	}
 }
 
 
 void move_right(t_cub3d *cub3d)
 {
-	cub3d -> user.x += cub3d -> user.v_dx * 0.1;
-	cub3d -> user.y += cub3d -> user.v_dy * 0.1;
+	int	nx;
+	int	ny;
+
+	nx = (int) (cub3d -> user.x + cub3d -> user.v_dx * 0.1 / cub3d -> map.tile_len);
+	ny = (int) (cub3d -> user.y + cub3d -> user.v_dy * 0.1 / cub3d -> map.tile_len);
+	if (cub3d -> map.array_map[ny][nx] != 1)
+	{
+		cub3d -> user.x += cub3d -> user.v_dx * 0.1;
+		cub3d -> user.y += cub3d -> user.v_dy * 0.1;
+	}
 }
 
 void move_left(t_cub3d *cub3d)
 {
-	cub3d -> user.x -= cub3d -> user.v_dx * 0.1;
-	cub3d -> user.y -= cub3d -> user.v_dy * 0.1;
+	int nx;
+	int ny;
+
+	nx = (int) (cub3d -> user.x - cub3d -> user.v_dx * 0.1 / cub3d -> map.tile_len);
+	ny = (int) (cub3d -> user.y - cub3d -> user.v_dy * 0.1 / cub3d -> map.tile_len);
+	if (cub3d -> map.array_map[ny][nx] != 1)
+	{
+		cub3d -> user.x -= cub3d -> user.v_dx * 0.1;
+		cub3d -> user.y -= cub3d -> user.v_dy * 0.1;
+	}
 }
 
 void	move(int key, t_cub3d *cub3d)
