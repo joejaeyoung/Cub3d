f1d5a1df5b425345ab975b438e1675359cf8a8a6
Jaeyoung Jo <jajo@c3r17s5.42gyeongsan.kr>
2024-08-18 20:22:42 +0900

    init

--- 3d.c
+++ 3d.c
@@ -1,6 +1,6 @@
 #include "Cub3d.h"
 
-void	ver_line(t_cub3d *cub3d, int x, double height, int color)
+void	ver_line(t_cub3d *cub3d, int x, double height, int color, int wall_dir)
 {
 	double	y1;
 	double	y2;
@@ -10,7 +10,8 @@ void	ver_line(t_cub3d *cub3d, int x, double height, int color)
 	while (y1 <= y2){
 		if (0 <= x && x <= WINDOW_WIDTH && \
 			0 <= y1 && y1 <= WINDOW_HEIGHT)
-		my_mlx_pixel_put(cub3d, round(x) , round(y1), color);
+		
+		my_mlx_pixel_put(cub3d, x , y1, cub3d->map.texture[wall_dir].texture[64 * (y1 % 64) + (x % 64)]);
 		y1++;
 	}
 }
